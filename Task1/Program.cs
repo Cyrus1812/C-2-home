9cbc00c04aa7d758f8f2e45a20bb927c1a343339
MAX <maksimdemidovru@mail.ru>
2022-08-24 01:03:28 +0300

    remove Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трёхзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int CalculationSekondNumber(int result)
+{
+    result = num% 100 /10;
+    return result;
+}
+
+
+int n = CalculationSekondNumber(num);
+
+Console.WriteLine(n);
+
+
+
+
+
