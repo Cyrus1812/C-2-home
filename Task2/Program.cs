cfe252c5a15e5622c7ff9f41d651346d4c86cc47
MAX <maksimdemidovru@mail.ru>
2022-08-26 00:54:26 +0300

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число ");
+string? num = Console.ReadLine();
+
+int CheckingTherdNumber(int result)
+{
+    result = Convert.ToInt32(new string(num[2], 1));
+    return result;
+
+}
+
+
+if (num.Length < 3)
+{
+    Console.WriteLine("нет 3 цифры");
+}
+else
+{
+    int n = CheckingTherdNumber(num[2]);
+    Console.WriteLine($"Третья цифра = {n}");
+}
+
+
