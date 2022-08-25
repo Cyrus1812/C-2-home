0d4cbf34a9fc28115156fe0ef6d4c3c98c38b61e
MAX <maksimdemidovru@mail.ru>
2022-08-26 00:58:45 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+string? num = Console.ReadLine();
+
+int s(int n)
+{
+    n = Convert.ToInt32(new string(num[0], 1));
+    return n;
+}
+
+int n = s(num[0]);
+
+if (n >= 6)
+{
+    Console.WriteLine("Цифра соответствует выходному дню");
+}
+else
+{
+    Console.WriteLine("Цифра не соответствует выходному дню");
+}
